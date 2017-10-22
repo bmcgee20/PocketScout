85140ee133f4eacc4ab158c2d988d491ca96b196
Jarrod <jarrod.mcgee@ttu.edu>
2017-10-22 15:42:56 -0500

    Hides all UI elements on add list
    When adding a new camping list item it hides the current list and everything else. When submit is done it opens them back up

--- Pocket_Scout/Pocket_Scout/CampingListPage.xaml.cs
+++ Pocket_Scout/Pocket_Scout/CampingListPage.xaml.cs
@@ -12,6 +12,8 @@ namespace Pocket_Scout
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CampingListPage : ContentPage
 	{
+        //TO DO: let the user delete and edit entries in the list view
+        //let a user cancel creating a new entry once they select creation
         List<CampingItem> testList = new List<CampingItem> {
                 new CampingItem{Item="Potato",Quantity="1 Bag",Person="John"},
                 new CampingItem{Item="Carrots",Quantity="1 Bag",Person="Bill"},
@@ -20,7 +22,6 @@ namespace Pocket_Scout
                 new CampingItem{Item="Sleeping Bag",Quantity="1 Each",Person="All"},
                 new CampingItem{Item="Axe",Quantity="1",Person="George"},
                 };
-        //TO DO: let the user delete entries in the list view
         public CampingListPage ()
 		{
 			InitializeComponent ();
@@ -36,6 +37,10 @@ namespace Pocket_Scout
             //item was not being added so add one here
             if (ItemBeingAdded == false)
             {
+                //make the rest of UI disappear for adding data
+                CampingListGrid.IsVisible = false;
+                CampingListDivider.IsVisible = false;
+                CampingListViewer.IsVisible = false;
                 //need to switch to item being added
                 ItemBeingAdded = true;
                 addImage.RelRotateTo(360, 500);
@@ -46,6 +51,7 @@ namespace Pocket_Scout
             else
             {
                 ItemBeingAdded = false;
+                
                 //Clicked the finished button so close out the stacklayout and add it to listview
                 StackAdd.IsVisible = false;
                 StackAdd.Padding = 0;
@@ -57,7 +63,29 @@ namespace Pocket_Scout
                 StackAddItem.Text = "";
                 StackAddPerson.Text = "";
                 StackAddQuantity.Text = "";
+                //turn back on the rest of the UI
+                StackAddItem.Unfocus(); StackAddPerson.Unfocus();StackAddQuantity.Unfocus();
+                CampingListGrid.IsVisible = true;
+                CampingListDivider.IsVisible = true;
+                CampingListViewer.IsVisible = true;
                 //add to databse here
+            }
+        }
+        //when you press enter or done in any of the adds it should move to next text box
+        void StackAddTextComplete(object sender, EventArgs args)
+        {
+            Entry currentEditor = (Entry)sender;
+            currentEditor.Unfocus();
+            if (currentEditor == StackAddItem)
+            {
+                StackAddQuantity.Focus();
+            }else if (currentEditor == StackAddQuantity)
+            {
+                StackAddPerson.Focus();
+            }
+            else
+            {
+                StackAddPerson.Unfocus();
             }
         }
     }
