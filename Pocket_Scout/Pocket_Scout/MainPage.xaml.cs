429b1f1caf75e1aae93ad8f5eb37316b396d32dc
Lino Virgen <lino.virgen96@gmail.com>
2017-10-22 18:26:29 -0500

    basic map functionality added
    map is available to see. one pin with description is added. NOTE: credentials need to be obtained by the user

--- Pocket_Scout/Pocket_Scout/MainPage.xaml.cs
+++ Pocket_Scout/Pocket_Scout/MainPage.xaml.cs
@@ -14,31 +14,9 @@ namespace Pocket_Scout
             InitializeComponent();
         }
 
-        private void ButtonClicked(object sender, EventArgs e)
-        {
-            string text = MainEntry.Text;
-
-            MainLabel.Text = "Hello " + text;
-        }
-
         private async void NavigateButtonClicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new TestPage());
-        }
-
-        private void GPSButtonClicked(object sender, EventArgs e)
-        {
-            /*
-             * Gets current location using xams.plugin.geolocator
-                var locator = CrossGeolocator.Current;
-                locator.DesiredAccuracy = 50;
-
-                var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
-
-                LongitudeLabel.Text = position.Longitude.ToString();
-                LatitudeLabel.Text = position.Latitude.ToString();
-
-            */
         }
     }
 }
