2c1c8869bf5206989e07283bb2740e9aabcb7c1e
Jarrod <jarrod.mcgee@ttu.edu>
2017-10-22 12:26:41 -0500

    Camping list add items
    allows the ui to let you add items to the camping list

--- Pocket_Scout/Pocket_Scout/CampingListPage.xaml.cs
+++ Pocket_Scout/Pocket_Scout/CampingListPage.xaml.cs
@@ -11,10 +11,25 @@ namespace Pocket_Scout
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CampingListPage : ContentPage
-	{
+	{   
+        //the current rows added through the button
+        int i = 2;
 		public CampingListPage ()
 		{
 			InitializeComponent ();
 		}
-	}
+        //When the plus button is clicked
+        void OnAdditionClicked(object sender, EventArgs args)
+        {
+            Image plus = (Image)sender;
+            //rotate image X degrees in 1 second
+            plus.RotateTo(360, 1000);
+            //Add a label for them to input their camping stuff here
+            CampingListGrid.Children.Add(new Editor { HorizontalOptions = LayoutOptions.FillAndExpand, VerticalOptions = LayoutOptions.Start }, 0, i);
+            CampingListGrid.Children.Add(new Editor { HorizontalOptions = LayoutOptions.FillAndExpand, VerticalOptions = LayoutOptions.Start }, 1, i);
+            CampingListGrid.Children.Add(new Editor { HorizontalOptions = LayoutOptions.FillAndExpand, VerticalOptions = LayoutOptions.Start }, 2, i);
+            i++;
+        }
+
+    }
 }
